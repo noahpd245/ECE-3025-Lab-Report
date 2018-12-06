fa11e1be48158b67dafd2cae69cf934916d7a024
Noah Daugherty <noahpd@gatech.edu>
2018-12-06 15:08:41 -0500

    I broke it

--- Matlab.m
+++ Matlab.m
@@ -1,17 +1,17 @@
 close all
 %% Conversions
 radians = (angle/360)*2*pi;
-nW = (mV - 5.1)*2;
-W = nW*10^(-9);
+V = (mV-5.1)*(10^-3);
+W = V*(.5*(10^-6));
 WNorm = W/max(W); %Ask buck about this
  
 %% Measured and Computed Parameters
-apetureRadius = .69/2*10^-3;
-sphericalRadius = (149.63+5.00)*10^-3;
-Ar = pi*(apetureRadius^2);
-P0 = max(W);
-I0 = P0/Ar;
-K0 = I0*(sphericalRadius^2);
+ apetureRadius = .69/2*10^-3;
+ sphericalRadius = (149.63+5.00)*10^-3;
+ Ar = pi*(apetureRadius^2);
+ P0 = max(W);
+ I0 = P0/Ar;
+ K0 = I0*(sphericalRadius^2);
  
 %% Least Squares fit 
 N = length(radians);
@@ -30,36 +30,27 @@ end
 powerBestFit = x(minIndex)
 smallestError = F_Least_Squares(minIndex)
 %% Integral
-angleIntegrand = radians(27:end);
-WIntegrand = W(27:end);
-integral_angle = ((WIntegrand*sphericalRadius^2)/Ar).*sin(angleIntegrand).*[diff(angleIntegrand); 0];
-Total_power = 2*pi*sum(integral_angle)
+angleIntegrand = radians(23:end);
+WIntegrand = W(23:end);
+Total_power_empirical = 2*pi*trapz(angleIntegrand,((WIntegrand*sphericalRadius^2)/Ar).*sin(angleIntegrand))
 Total_power_predicted = (2*pi*K0)/(powerBestFit +1)
+Actual_Power_Consumed = (9-6.54)*(6.54/1182)
 
- %% Plots
- 
-% Radians vs. Power Emitted
-%Rad_vs_Power = figure;
-%axes1 = axes('Parent',Rad_vs_Power);
-%hold(axes1,'on');
-%plot(radians,WNorm);
-%hold on
-%plot(radians,cos(radians).^powerBestFit);
-%ylabel({'Power(Watts)'});
-%xlabel({'Radians'});
-%title({'Radians vs. Power Emitted'});
-%hold off
+%% Re-Fitted Model
+N = length(radians);
+x = 0:.1:200;
+tempFitNew = zeros(N,1);
+F_Least_Squares_New = zeros(length(x),1);
 
-% Least Squares Plot
-figure2 = figure;
-plot(x,F_Least_Squares)
+for index = 1:length(x)
+%index = 2;
+for i = 1:N
+ tempFitNew(i) = (WNorm(i) - ((cos(radians(i)-.1396).^x(index))+(cos(radians(i)+.1396).^x(index))) ).^2;
+end
+F_Least_Squares_New(index) = (1/N)*sum(tempFitNew);
+end
+[minValNew minIndexNew] = min(F_Least_Squares_New);
+powerBestFitNew = x(minIndexNew)
+smallestErrorNew = F_Least_Squares_New(minIndexNew)
 
-% Radians vs. Power Emitted polar plot
-figure3 = figure;
-%axes1 = axes('Parent',Rad_vs_Power);
-radiansplot = radians;
-polarplot(radiansplot,WNorm);
-hold on
-polarplot(radians,cos(radians).^powerBestFit);
-hold off
-title({'Radians vs. Power Emitted Polar'});
+plot(radians,K0*((cos(radians-.1396).^powerBestFitNew)+(cos(radians+.1396).^powerBestFitNew)),radians,((W*sphericalRadius^2)/Ar))
